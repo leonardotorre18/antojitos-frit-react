5dc0ccbad6bc9a0cba89b8c114ee9d43e4bde5db
Leonardotorre18 <leonardotorre14@gmail.com>
2023-03-30 12:02:34 -0400

    Backend: Cors instalado y configurado para todas las peticiones

--- server/src/server/index.ts
+++ server/src/server/index.ts
@@ -1,6 +1,9 @@
 import express, { Express, Request, Response } from "express";
+import cors from "cors";
 
 const server: Express = express();
+
+server.use(cors())
 
 server.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
