f534c1ad66c6c695ef78a0474fe4898bb03d32eb
Leonardotorre18 <Leonardotorre14@gmail.com>
2023-06-08 19:41:40 -0400

    Test para React-Redux agregados

--- client/src/test/App.test.tsx
+++ client/src/test/App.test.tsx
@@ -2,8 +2,17 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 import { expect, test } from 'vitest'
 import App from '../App'
+import configureStore from 'redux-mock-store'
+import {Provider} from 'react-redux'
+import applicationStore from '../store'
 
 test('Aplicación se renderiza correctamente', () => {
-  render(<App />)
+  const mockStore = configureStore();
+  const store = mockStore(applicationStore.getState())
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  )
   expect(screen).toBeDefined()
 })
