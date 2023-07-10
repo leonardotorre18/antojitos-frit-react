7b12f06f1dbea30929d79225b40f06f27898326e
Leonardotorre18 <Leonardotorre14@gmail.com>
2023-07-09 20:45:13 -0400

    Test de App corregido

--- client/src/test/App.test.tsx
+++ client/src/test/App.test.tsx
@@ -1,18 +1,14 @@
-import React from 'react'
+
 import { render, screen } from '@testing-library/react'
 import { expect, test } from 'vitest'
 import App from '../App'
-import configureStore from 'redux-mock-store'
-import {Provider} from 'react-redux'
-import applicationStore from '../store'
+import Providers from '../Providers';
 
 test('Aplicación se renderiza correctamente', () => {
-  const mockStore = configureStore();
-  const store = mockStore(applicationStore.getState())
   render(
-    <Provider store={store}>
+    <Providers>
       <App />
-    </Provider>
+    </Providers>
   )
   expect(screen).toBeDefined()
 })
